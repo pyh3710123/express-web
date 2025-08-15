99723acc88c1bd2b86083a83a982452a8ba92c74
zhoutong <zhout7449@gmail.com>
2025-08-15 17:23:17 +0700

    代码提交

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
-module.exports = router;
+
+// 导入express
+const express = require('express')
+// 创建路由对象
+const router = express.Router()
+
+// 导入路由处理函数
+const userRouterHandler = require('../controllers/users')
+
+// 获取用户列表的路由
+router.get('/userlist',userRouterHandler.getUserListHandle)
+
+// 更新用户信息的路由
+router.put('/userinfo',userRouterHandler.updateUserInfoHandle)
+
+// 向外导出路由对象
+module.exports = router
